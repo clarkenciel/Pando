a70c8787f49182b8dd56db8d29a33dff30c4d131
clarkenciel <danny@clarkenciel.com>
2016-04-26 09:09:26 -0700

    upping gain again

--- resources/pando/scripts/source.js
+++ resources/pando/scripts/source.js
@@ -323,7 +323,7 @@ Room.participantSoundSetup = function (app) {
     then(function (resp) {
       var freq = ST.coordToFrequency(resp.fundamental, resp.dimensions, resp.coord);
       cracked("#participant").frequency(freq);
-      cracked('#master').ramp(0.3,0.1,'gain',0.0);
+      cracked('#master').ramp(0.5,0.1,'gain',0.0);
       cracked.loop("start");
             
       app.soundCallback = participantCallback;
